a0ed68ffc30da12030be161c54de189f4632a441
Alvaro S. P. Silva <alvaro123sps@gmail.com>
2023-04-19 21:28:02 -0300

    Added persistence between sesssions with redux-persist lib

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = document.getElementById('root');
 
@@ -10,7 +11,9 @@ if (root) {
     const container = createRoot(root);
     container.render(
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     );
 }
